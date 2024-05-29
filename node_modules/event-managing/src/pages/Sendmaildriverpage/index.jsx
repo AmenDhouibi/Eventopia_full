5a67c5d394c1af417e26447b351039f7770a7560
Nourhen K <120370791+nourhen369@users.noreply.github.com>
2024-05-29 23:46:04 +0100

    mails pages

--- node_modules/event-managing/src/pages/Sendmaildriverpage/index.jsx
+++ node_modules/event-managing/src/pages/Sendmaildriverpage/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Helmet } from "react-helmet";
 import { Button, Img, Text, Heading } from "../../components";
 
-export default function SendmaildriverpagePage() {
+export default function SendmailguestpagePage() {
   const [emailContent, setEmailContent] = useState(`Greetings,
   I'm Event_manager, the Event Manager at Event_name. We would be thrilled to have you participate in our upcoming event, on Event_date at Event_location.
   Highlights:
@@ -47,78 +47,82 @@ export default function SendmaildriverpagePage() {
         {/* header section */}
         <div className="absolute bottom-0 left-[2%] top-0 my-auto flex h-max w-[68%] flex-col items-start gap-[22px]">
           {/* logo section */}
-          <div className="relative h-[85px] w-[26%]">
-            <Img
-              src="images/img_header_logo.png"
-              alt="header logo"
-              className="absolute bottom-[-0.51px] right-[0.25px] m-auto h-[60px] w-[267px] object-contain"
-            />
-          </div>
+          <div className="relative mb-[7px] h-[50px] w-[15%] sm:w-full">
+          <Img
+            src="images/img_frame.svg"
+            alt="image"
+            className="absolute left-0 top-0 m-auto h-[45px] w-[45px]"
+          />
+          <Text size="lg" as="p" className="absolute bottom-[-0.51px] right-0.25 m-auto text-center lowercase">
+            eventopia
+          </Text>
+        </div>
 
           {/* email content section */}
-          <div className="flex w-[90%] flex-col items-start justify-center self-end rounded-[30px] bg-blue_gray-100_7f px-[54px] pb-[121px] pt-[30px] shadow-xs md:w-full md:px-5 md:pb-5 sm:p-5">
-            <Heading as="h1" className="ml-1.5 uppercase md:ml-0">
-              start choosing your guests
+          <div className="flex w-[90%] flex-col items-start justify-center self-end rounded-[30px] bg-blue_gray-100_7f px-[30px] pb-[121px] pt-[30px] shadow-xs md:w-full md:px-5 md:pb-5 sm:p-5">
+            <Heading as="h1" className="ml-1.5 italic md:ml-0">
+              Start choosing your drivers
             </Heading>
             <Text className="text-shadow-ts mt-[26px] flex items-center justify-center rounded-[30px] bg-blue_gray-100 px-[35px] pb-[17px] pt-[13px] sm:px-5">
-              To:
+              To:&nbsp;
               <textarea
                 value={recipientEmail}
                 onChange={handleEmailChange}
                 placeholder="Type an email"
+                style={{ height: '32px', width: '800px', overflow: 'hidden' }}
               />
+              <Button className="mt-2" onClick={addEmail}>
+                Add Email
+              </Button>
             </Text>
-            <Button className="mt-2" onClick={addEmail}>
-              Add Email
-            </Button>
-            <Text className="text-shadow-ts mt-2 flex items-center justify-center rounded-[30px] bg-blue_gray-100 px-[35px] py-[15px] sm:px-5">
+            
+            {/* email object section */}
+            <Text className="text-shadow-ts mt-[26px] flex items-center justify-center rounded-[30px] bg-blue_gray-100 px-[35px] pb-[17px] pt-[13px] sm:px-5">
               <span className="text-blue_gray-900">Object : Invitation to Join US at&nbsp;</span>
               <span className="italic text-gray_600">Event_name</span>
             </Text>
 
             {/* email body section */}
-            <div className="mx-auto mt-[9px] flex w-full max-w-[930px] self-stretch rounded-[30px] bg-blue_gray-100 pb-[38px] pl-[39px] pr-9 pt-[39px] shadow-xs md:p-5">
-              <label htmlFor="email" className="form-label">
-                Email Content:
-              </label>
+            <div className="mx-auto mt-[9px] flex w-full max-w-[1000px] self-stretch rounded-[30px] bg-blue_gray-100 pb-[38px] pl-[39px] pr-9 pt-[39px] shadow-xs md:p-5">
               <textarea
                 className="form-control w-full mt-2 p-2 border border-gray-300 rounded"
                 id="email"
                 rows="8"
                 value={emailContent}
                 onChange={handleTextareaChange}
+                style={{ height: '320px' }}
               ></textarea>
             </div>
             <Button
-                size="sm"
-                onClick={handleSave}
-                className="mt-4"
-              >
-                Save Template
-              </Button>
+              size="sm"
+              onClick={handleSave}
+              className="mt-4"
+            >Save Template</Button>
           </div>
         </div>
 
         {/* selected staff section */}
-        <div className="absolute bottom-0 right-[7%] top-0 my-auto h-[782px] w-[32%]">
+        <div className="absolute bottom-0 right-[5%] top-0 my-auto h-[782px] w-[0%]">
           
           <div className="absolute bottom-[0.00px] right-[0.00px] m-auto flex flex-col items-start justify-center rounded-[30px] bg-blue_gray-100_7f px-[22px] pb-[441px] pt-[55px] shadow-xs md:py-5 sm:p-5">
-            <Heading as="h2" className="ml-[31px] uppercase md:ml-0">
-              selected guests
-            </Heading>
-            {/* selected staff*/}
-            {selectedEmails.map((email, index) => (
-              <Text key={index} className="text-shadow-ts mt-12 flex items-center justify-center rounded-[30px] bg-blue_gray-100 px-[35px] py-4 underline sm:px-5">
-                {email}
-              </Text>
-            ))}
+            <Heading as="h1" className="ml-1.5 italic md:ml-0">
+              Drivers
+            </Heading> 
+            <div className="mt-4 flex flex-col gap-4 overflow-y-auto max-h-[400px] w-full">
+              {selectedEmails.map((email, index) => (
+                <Text key={index} className="text-shadow-ts flex items-center justify-center rounded-[30px] bg-blue_gray-100 px-5 py-4 underline">
+                  {email}
+                </Text>
+              ))}
+          </div>
+
           </div>
           <Button
-          size="xs"
-          className="absolute bottom-[12%] right-[7%] m-auto min-w-[511px] gap-[35px] font-semibold sm:px-5"
-        >
+            size="sm"
+            className="absolute bottom-[12%] right-[0%] m-auto min-w-[400px] gap-[5px] font-semibold sm:px-5"
+          >
           Send mail
-        </Button>
+          </Button>
         </div>
 
         {/* send button section */}
