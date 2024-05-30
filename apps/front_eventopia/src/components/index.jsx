a79a1d35d4a151dc5f3ed26ddd666f4abd073db4
AmenDhoibi <amen_dhouibi@yahoo.com>
2024-05-30 23:41:22 +0100

    fixed_signin/up_and_creatingevent

---
+++ apps/front_eventopia/src/components/index.jsx
@@ -0,0 +1,7 @@
+import { Text } from "./Text";
+import { Heading } from "./Heading";
+import { Img } from "./Img";
+import { Button } from "./Button";
+import { Input } from "./Input";
+import { DatePicker } from "./DatePicker";
+export { Text, Heading, Img, Button, Input, DatePicker };
