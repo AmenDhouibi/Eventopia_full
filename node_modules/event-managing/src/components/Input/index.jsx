cab74cec019bb6783b7b4f9beeb448badbda0497
AmenDhoibi <amen_dhouibi@yahoo.com>
2024-06-01 02:15:22 +0100

    fixed_merged_conflicts

--- node_modules/event-managing/src/components/Input/index.jsx
+++ node_modules/event-managing/src/components/Input/index.jsx
@@ -45,7 +45,7 @@ const Input = React.forwardRef(
         >
           {!!label && label}
           {!!prefix && prefix}
-          <input ref={ref} type={type} name={name} onChange={handleChange} placeholder={placeholder} {...restProps} />
+          <input ref={ref} type={type} name={name} onChange={(e) => onChange(e)} placeholder={placeholder} {...restProps} />
           {!!suffix && suffix}
         </div>
       </>
